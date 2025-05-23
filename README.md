e2afbde01ed0baa24acbb092da3351fe8981b3fd
Bakuwan <f4dhliraihan@gmail.com>
2025-05-23 21:42:38 +0700

    Experiment 3.2: Be Creative!

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-![image](images/image.png)
+![image](images/image1.png)
+
+---
+![image](images/image2.png)
+![image](images/image3.png)
+Saya menambahkan sebuah welcome text di halaman login dan warna gradient di halaman login. Saya juga menambahkan efek greyed-out pada tombol ketika input username belum diisi (disabled state) serta efek hover halus dengan perubahan warna dan bayangan
--- src/components/login.rs
+++ src/components/login.rs
@@ -1,4 +1,4 @@
-use web_sys::HtmlInputElement;
+use web_sys::{HtmlInputElement, InputEvent};
 use yew::functional::*;
 use yew::prelude::*;
 use yew_router::prelude::*;
@@ -13,7 +13,6 @@ pub fn login() -> Html {
 
     let oninput = {
         let current_username = username.clone();
-
         Callback::from(move |e: InputEvent| {
             let input: HtmlInputElement = e.target_unchecked_into();
             current_username.set(input.value());
@@ -27,13 +26,27 @@ pub fn login() -> Html {
     };
 
     html! {
-       <div class="bg-gray-800 flex w-screen">
-            <div class="container mx-auto flex flex-col justify-center items-center">
-                <form class="m-4 flex">
-                    <input {oninput} class="rounded-l-lg p-4 border-t mr-0 border-b border-l text-gray-800 border-gray-200 bg-white" placeholder="Username" />
-                    <Link<Route> to={Route::Chat}> <button {onclick} disabled={username.len()<1} class="px-8 rounded-r-lg bg-violet-600	  text-white font-bold p-4 uppercase border-violet-600 border-t border-b border-r" >{"Go Chatting!"}</button></Link<Route>>
-                </form>
-            </div>
+      <div class="bg-gradient-to-r from-purple-600 via-pink-500 to-red-500 flex w-screen h-screen items-center justify-center">
+        <div class="bg-white bg-opacity-20 backdrop-blur-md p-8 rounded-xl shadow-lg">
+          <h1 class="text-3xl font-bold text-white mb-6">{"Welcome to YewChat!"}</h1>
+          <form class="m-4 flex">
+            <input {oninput}
+                   class="bg-white rounded-l-lg p-4 border-t mr-0 border-b border-l text-gray-800 border-gray-200 outline-none focus:ring-2 focus:ring-purple-400 transition"
+                   placeholder="Username" />
+            <Link<Route> to={Route::Chat}>
+                <button {onclick}
+                        disabled={username.len() < 1}
+                        class="
+                            px-8 rounded-r-lg bg-violet-600 text-white font-bold p-4 uppercase
+                            transition duration-300 ease-in-out transform
+                            hover:bg-violet-700 hover:shadow-lg hover:-translate-y-1
+                            disabled:opacity-50
+                        ">
+                    {"Go Chatting!"}
+                </button>
+            </Link<Route>>
+          </form>
         </div>
+      </div>
     }
 }
